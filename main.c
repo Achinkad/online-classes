23368d68d35785e03d08ad269bff4a4b465efad4
João Adão <2200658@my.ipleiria.pt>
2020-12-30 10:46:19 +0000

    commit windows-1252

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include "funcoes_auxiliares.h"
 
 int main()
 {
-    printf("Hello JosÃ© Areia!\n");
+    setlocale(LC_ALL, "");    // para o texto sair bem (mesmo os acentos)
+
+    printf("Hello José Areia!\n");
+
     return 0;
 }
