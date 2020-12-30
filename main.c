b3c6931dcc30e2437bb18528546f20fa4a0699be
José Areia <jose.apareia@gmail.com>
2020-12-30 12:11:36 +0000

    Continução da estrutura do menu inicial

--- main.c
+++ main.c
@@ -10,15 +10,50 @@ int main()
     char opcao;
     setlocale(LC_ALL, "");
 
-    printf("(G)estao de Unidades Curriculares\n");
-    scanf("%c", &opcao);
-    limpaBufferStdin();
-    opcao = toupper(opcao);
+    do
+    {
+        printf("--------- Menu Principal ---------\n\n");
+        printf("Unidade Curriculares: 12\tAulas agendadas: 12\n");
+        printf("Aulas realizadas: 21\t\tAulas gravadas: 3\n\n");
+        printf("A - Gestao de Unidades Curriculares\nB - Gestao de Aulas Online\nC - Gestao de Ficheiros\n");
+        printf("D - Gestao de Dados Estatisticos\nE - Gestao de Acesso as Aulas Online\n");
+        printf("S - Sair\n");
+        printf("\nInsira uma opcao: ");
+        scanf("%c", &opcao);
+        limpaBufferStdin();
+        opcao = toupper(opcao);
 
-    switch(opcao){
-        case 'G': printf("test");
-        break;
+        switch(opcao)
+        {
+        case 'A':
+            printf("Gestao de Unidades Curriculares\n\n");
+            break;
+
+        case 'B':
+            printf("Gestao de Aulas Online\n\n");
+            break;
+
+        case 'C':
+            printf("Gestao de Ficheiros\n\n");
+            break;
+
+        case 'D':
+            printf("Gestao de Dados Estatisticos\n\n");
+            break;
+
+        case 'E':
+            printf("Gestao de Acesso as Aulas Online\n\n");
+            break;
+
+        case 'S':
+            return 0;
+            break;
+
+        default:
+            printf("Opcao invalida. Por favor, introduza uma opcao valida.\n\n");
+            break;
+        }
     }
-
+    while(opcao != 'S');
     return 0;
 }
