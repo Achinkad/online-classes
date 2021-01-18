46c49c2bcff76d5fcdc7d9f9617428597aa4bad3
José Areia <jose.apareia@gmail.com>
2021-01-18 08:48:37 +0000

    Atualização na estrutura do menu

--- main.c
+++ main.c
@@ -20,3 +20,61 @@ int main()
 
     return 0;
 }
+
+// Apresentação da estrutura do menu
+char menu()
+{
+    char opcao;
+    printf("------------------ Menu Principal ------------------\n\n");
+    printf("Unidade Curriculares: 12\tAulas agendadas: 12\n");
+    printf("Aulas realizadas: 21\t\tAulas gravadas: 3\n\n");
+    printf("\tA - Gestao de Unidades Curriculares\n\tB - Gestao de Aulas Online\n\tC - Gestao de Ficheiros\n");
+    printf("\tD - Gestao de Dados Estatisticos\n\tE - Gestao de Acesso as Aulas Online\n");
+    printf("\tS - Sair\n");
+    printf("\nInsira uma opcao: ");
+    scanf("%c", &opcao);
+    limpaBufferStdin();
+    opcao = toupper(opcao);
+    return opcao;
+}
+
+// Verifica a opção escolhida na função "menu"
+void verificaOpcao(char opcao, tipoUC ucs[MAX_UC], int quantUC)
+{
+    if(opcao == 'A' || opcao == 'B' || opcao == 'C' || opcao == 'D' || opcao == 'E' || opcao == 'S')
+    {
+        switch(opcao)
+        {
+        case 'A':
+            gestaoUCs(ucs,quantUC);
+            break;
+
+        case 'B':
+            printf("Gestao de Aulas Online\n\n");
+            break;
+
+        case 'C':
+            printf("Gestao de Ficheiros\n\n");
+            break;
+
+        case 'D':
+            printf("Gestao de Dados Estatisticos\n\n");
+            break;
+
+        case 'E':
+            printf("Gestao de Acesso as Aulas Online\n\n");
+            break;
+
+        case 'S':
+            break;
+        }
+    }
+    else
+    {
+        printf("Opcao invalida. Por favor, insira uma opcao valida: ");
+        scanf("%c", &opcao);
+        limpaBufferStdin();
+        opcao = toupper(opcao);
+        verificaOpcao(opcao, ucs, quantUC);
+    }
+}
